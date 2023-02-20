90f5c53078e7fa095e34f6162e92afc943ccf421
gsalmao <gustmsalmeron@gmail.com>
2023-02-20 18:00:23 -0300

    Sprites + Sprite Swap System

---
+++ Assets/Scripts/Animation/SpriteSwapper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace PocketPeople.SpriteSwap
+{
+
+    /// <summary>
+    /// Allows to set and change sprites at will, without the need of an animation clip for each sprite.
+    /// Use the Animator to change the spriteIndex and change the SpriteSwapperData to change the sprite.
+    /// </summary>
+    public class SpriteSwapper : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private SpriteSwapperData data;
+        public int spriteIndex;
+
+        private void FixedUpdate() => spriteRenderer.sprite = data?.Sprites[Mathf.Clamp(spriteIndex, 0, data.Sprites.Length - 1)];
+
+    }
+}
